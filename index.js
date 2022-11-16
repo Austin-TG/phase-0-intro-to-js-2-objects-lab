3fcf9b565bb25af2afe3c4dfda87c377b9fe1650
Austin Griffin <austintg94@gmail.com>
2022-11-15 18:03:37 -0700

    commit tree f677060995f520461a70f7d904e1697be7a9bdb8

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: "Austin",
+    streetAddress: "Bilbao Street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    const newEmployee = {...employee};
+
+    newEmployee[key] = value;
+    
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+    employee[key] = value;
+
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key)
+{
+    const newEmployeeTwo = {...employee};
+
+    delete newEmployeeTwo[key];
+
+    return newEmployeeTwo;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+    delete employee[key];
+
+    return employee;
+}
